eadf9d019de014ebbb32d89685897358172f8261
Michael <michael.d.lange88@gmail.com>
2022-10-19 21:24:07 -0700

    commit tree 067af40a46010bdf7cea9787571b7bad66668a86

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let newText = document.querySelector("p")
+    newText.textContent = "This is really cool!"
+  });
